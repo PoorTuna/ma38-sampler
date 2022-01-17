fe46174c874fac56510ae4f0ab84781fada11536
Oren <orengamingil@gmail.com>
2022-01-17 08:30:14 +0200

    Added parser example structures

---
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/parser/parserExample/JsonParser.java
@@ -0,0 +1,12 @@
+package workspace.mil.matmon.data.exercise.sampler.parser.parserExample;
+
+import workspace.mil.matmon.data.exercise.sampler.parser.Parser;
+
+import java.util.List;
+
+public class JsonParser extends Parser {
+    @Override
+    public List<String[]> read(String path) {
+        return null;
+    }
+}
