fb49d90eddeef197f647d03980bf657f08cdd4ec
Oren <orengamingil@gmail.com>
2022-01-17 08:41:45 +0200

    Implemented a CSVParser

--- src/main/java/workspace/mil/matmon/data/exercise/sampler/parser/Parser.java
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/parser/Parser.java
@@ -1,8 +1,12 @@
 package workspace.mil.matmon.data.exercise.sampler.parser;
 
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public abstract class Parser {
-    public abstract List<String[]> read(String path);
+    public abstract List<String[]> read(String path) throws IOException, CsvValidationException;
 
 }
--- src/main/java/workspace/mil/matmon/data/exercise/sampler/parser/parserExample/CSVParser.java
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/parser/parserExample/CSVParser.java
@@ -1,12 +1,26 @@
 package workspace.mil.matmon.data.exercise.sampler.parser.parserExample;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import workspace.mil.matmon.data.exercise.sampler.parser.Parser;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CSVParser extends Parser {
     @Override
-    public List<String[]> read(String path) {
-        return null;
+    public List<String[]> read(String path) throws IOException, CsvValidationException {
+        List<String[]> newFile = new ArrayList<String[]>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(path))) {
+            String[] lineInArray;
+            while ((lineInArray = reader.readNext()) != null) {
+                newFile.add(lineInArray);
+            }
+        }
+
+        return newFile;
     }
 }
