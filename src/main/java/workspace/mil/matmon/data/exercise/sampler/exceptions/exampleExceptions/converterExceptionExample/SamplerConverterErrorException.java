15f49c322b8de67f2173468c8fd6b1152a78f657
Oren <orengamingil@gmail.com>
2022-01-18 09:03:15 +0200

    Added common converter exceptions

---
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/exceptions/exampleExceptions/converterExceptionExample/SamplerConverterErrorException.java
@@ -0,0 +1,9 @@
+package workspace.mil.matmon.data.exercise.sampler.exceptions.exampleExceptions.converterExceptionExample;
+
+import workspace.mil.matmon.data.exercise.sampler.exceptions.SamplerException;
+
+public class SamplerConverterErrorException extends SamplerException {
+    public SamplerConverterErrorException(String str) {
+        super(str);
+    }
+}
---
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/exceptions/exampleExceptions/converterExceptionExample/examples/SamplerInvalidInputErrorException.java
@@ -0,0 +1,9 @@
+package workspace.mil.matmon.data.exercise.sampler.exceptions.exampleExceptions.converterExceptionExample.examples;
+
+import workspace.mil.matmon.data.exercise.sampler.exceptions.exampleExceptions.converterExceptionExample.SamplerConverterErrorException;
+
+public class SamplerInvalidInputErrorException extends SamplerConverterErrorException {
+    public SamplerInvalidInputErrorException(String str) {
+        super(str);
+    }
+}
