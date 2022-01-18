a40a16b5f9f2def8c79160e436938142536e5c1d
Oren <orengamingil@gmail.com>
2022-01-18 10:05:35 +0200

    Added annotations to clean some code

--- src/main/java/workspace/mil/matmon/data/exercise/sampler/etl/etl/ETL.java
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/etl/etl/ETL.java
@@ -1,9 +1,14 @@
 package workspace.mil.matmon.data.exercise.sampler.etl.etl;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import workspace.mil.matmon.data.exercise.sampler.converter.Converter;
 import workspace.mil.matmon.data.exercise.sampler.parser.Parser;
 import workspace.mil.matmon.data.exercise.sampler.writer.Writer;
 
+@AllArgsConstructor
+@Data
 public class ETL {
     private Parser parser;
     private Writer writer;
--- src/main/java/workspace/mil/matmon/data/exercise/sampler/etl/etlManager/ETLManager.java
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/etl/etlManager/ETLManager.java
@@ -1,10 +1,14 @@
 package workspace.mil.matmon.data.exercise.sampler.etl.etlManager;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import workspace.mil.matmon.data.exercise.sampler.etl.etl.ETL;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Data
 public class ETLManager {
     public List<ETL> etlManager;
-    
+
 }
