37723b70d7bd00924e50ec8c7f867a1392d984d8
Oren <orengamingil@gmail.com>
2022-01-18 08:51:07 +0200

    Added basic custom exception

---
+++ src/main/java/workspace/mil/matmon/data/exercise/sampler/exceptions/SamplerException.java
@@ -0,0 +1,8 @@
+package workspace.mil.matmon.data.exercise.sampler.exceptions;
+
+public class SamplerException extends Exception{
+    public SamplerException (String str) {
+        super(str);
+    }
+
+}
